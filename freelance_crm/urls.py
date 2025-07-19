d19f428b2c856906cfb12365e42c02c21a3515db
AnnaValeryevna <gorbunova@informatics.ru>
2025-07-19 14:44:29 +0300

    Main template done

--- freelance_crm/urls.py
+++ freelance_crm/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from core.views import index,about
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("about", about, name="about"),
 ]
