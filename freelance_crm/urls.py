32614f264f7d411f0411ebba2d6912a22ffc3dd8
AnnaValeryevna <gorbunova@informatics.ru>
2025-07-19 15:52:41 +0300

    Login Signup and logout done

--- freelance_crm/urls.py
+++ freelance_crm/urls.py
@@ -18,9 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import index,about
+from userprofile.views import signup
+from django.contrib.auth import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", index, name="index"),
-    path("about", about, name="about"),
+    path("about/", about, name="about"),
+    path("sign-up/", signup, name="signup"),
+    path("log-in/", views.LoginView.as_view(template_name="userprofile/login.html"), name="login"),
+    path("log-out/", views.LogoutView.as_view(), name="logout"),
+    path("admin/", admin.site.urls),
 ]
